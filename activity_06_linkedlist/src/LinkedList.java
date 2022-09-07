f8837be96b122e05188a94aa7c117d9eceed33c6
Thyago Mota <thyagomota@gmail.com>
2022-09-07 09:25:54 -0600

    updates

---
+++ activity_06_linkedlist/src/LinkedList.java
@@ -0,0 +1,8 @@
+/*
+ * CS2050 - Computer Science II - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 06 - LinkedList
+ */
+
+public class LinkedList {
+}
---
+++ activity_06_linkedlist/src/Node.java
@@ -0,0 +1,10 @@
+/*
+ * CS2050 - Computer Science II - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 06 - Node
+ */
+
+public class Node {
+
+    
+}
