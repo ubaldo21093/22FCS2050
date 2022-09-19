5eea8e6fee1d330a338d793be1ff3730dd6da0d0
Thyago Mota <thyagomota@gmail.com>
2022-09-19 09:33:23 -0600

    updates

---
+++ activity_07_stack/src/Node.java
@@ -0,0 +1,9 @@
+/*
+ * CS2050 - Computer Science II - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 07 - Node
+ */
+
+public class Node<E> { 
+
+}
---
+++ activity_07_stack/src/Stack.java
@@ -0,0 +1,8 @@
+/*
+ * CS2050 - Computer Science II - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 07 - Stack
+ */
+
+public class Stack<E> {
+}
