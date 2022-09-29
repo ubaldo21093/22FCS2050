8b154e6fc3f63c0d54a18daceaa52ad66b4316d9
Thyago Mota <thyagomota@gmail.com>
2022-09-29 09:24:07 -0600

    updates

---
+++ activity_10_queue/src/Queue.java
@@ -0,0 +1,17 @@
+/*
+ * CS2050 - Computer Science II - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 10 - Queue
+ */
+
+public class Queue<E> extends LinkedList<E> {
+
+    public void push(E value) {
+
+    }
+
+    public E pop() throws Exception {
+        return null;
+    }
+
+}
---
+++ activity_10_queue/src/QueueDriver.java
@@ -0,0 +1,12 @@
+/*
+ * CS2050 - Computer Science II - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 10 - QueueDriver
+ */
+
+public class QueueDriver {
+
+    public static void main(String[] args) {
+        
+    }
+}
