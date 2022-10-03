10ff68b0323ad725712d230619c77400b6ea60a3
Thyago Mota <thyagomota@gmail.com>
2022-10-03 16:03:28 -0600

    updates

---
+++ activity_14_exhaustive_search/README.md
@@ -0,0 +1,3 @@
+# Instructions  
+
+Draw the “search tree” for all of the possible solutions of the problem “search for all of the combinations of the letters a, b, and c with a size 3”. Draw two search trees, one using a **queue** and another one using a **stack**.  Write a sequential number next to a letter every time you push it to the queue/stack, respectively.  Write a check mark next to a letter every time you pop it from the queue/stack, respectively.  Remember that the letters get reversed when they are pushed onto the stack. At the end, describe how different the two implementations are and which one do you think is preferable?  When you are done, finish the implementation of the **exhaustive search** program. 
---
+++ activity_14_exhaustive_search/src/ExhaustiveSearch.java
@@ -0,0 +1,34 @@
+/*
+ * CS 2050 - Computer Science II - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 14 - Exhaustive Search
+ */
+
+// Goal: to search for all of the combinations of letters with a given size
+public class ExhaustiveSearch {
+
+    static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    static void search(int n) {
+
+        // TODO: create a stack of String objects
+
+        // TODO: push all letters of the alphabet onto the stack
+
+
+        // TODO: loop until the stack is empty
+
+            // TODO: pop a String from the stack
+
+            // TODO: IF size of the String is “n”, print the String
+
+
+            // TODO: ELSE, push back all of the combinations of the String with each individual letters of the alphabet
+
+
+    }
+
+    public static void main(String[] args) {
+        search(3);
+    }
+}
