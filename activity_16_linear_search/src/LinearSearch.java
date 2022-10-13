bd3c3c5e7c10d77442c7c39c15408f73d62464a3
Thyago Mota <thyagomota@gmail.com>
2022-10-13 16:14:29 -0600

    updates

--- activity_16_linear_search/src/LinearSearch.java
+++ activity_16_linear_search/src/LinearSearch.java
@@ -7,19 +7,26 @@
 public class LinearSearch {
 
     static boolean lsearch(int data[], int el) {
-
+        for (int i = 0; i < data.length; i++)
+            if (data[i] == el)
+                return true;
         return false;
     }
 
     static boolean lsearch2(int data[], int el) {
-
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el)
+                return true;
+            if (data[i] > el)
+                break;
+        }
         return false;
     }
 
     public static void main(String[] args) {
-        int data[] = { 3, 5, 1, 8, 12, 2, 7, 9 };
-//        int data[] = { 1, 2, 3, 5, 7, 8, 9, 12 };
-        if (lsearch2(data, 4))
+//        int data[] = { 3, 5, 1, 8, 12, 2, 7, 9 };
+        int data[] = { 1, 2, 3, 5, 7, 8, 9, 12 };
+        if (lsearch2(data, 8))
             System.out.println("Found!");
         else
             System.out.println("Not found!");
