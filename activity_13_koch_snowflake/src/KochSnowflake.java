b93be791713ef5d357f804f5d744d26cdb37f7c7
Thyago Mota <thyagomota@gmail.com>
2022-10-14 14:45:56 -0600

    updates

--- activity_13_koch_snowflake/src/KochSnowflake.java
+++ activity_13_koch_snowflake/src/KochSnowflake.java
@@ -1,5 +1,5 @@
 /*
- * CS 2050 - Computer Science II - Fall 2022
+ * CS 2050 - Computer Science II - Summer 2021
  * Instructor: Thyago Mota
  * Description: Activity 13 - KochSnowflake class
  */
@@ -15,33 +15,71 @@ public class KochSnowflake extends JPanel {
     private double direction; // remember: this value is in degrees (not radians!)
     private Color  color;
 
+    // TODO: define appropriate class constants here
     static final int    MIN_DEPTH     = 0;
     static final double MIN_SIZE     = 10;
 
     KochSnowflake(int xPixel, int yPixel, int depth, double size, Color color) {
         super();
 
-        // TODO: finish implementation according to instructions
-
+        // TODO: finish implementation
+        if (xPixel < 0)
+            this.xPixel = 0;
+        else
+            this.xPixel = xPixel;
+        if (yPixel < 0)
+            this.yPixel = 0;
+        else
+            this.yPixel = yPixel;
+        if (depth < MIN_DEPTH)
+            this.depth = MIN_DEPTH;
+        else
+            this.depth = depth;
+        if (size < MIN_SIZE)
+            this.size = MIN_SIZE;
+        else
+            this.size = size;
+        this.direction = 0;
+        this.color = color;
     }
 
-    // TODO: implement helper method according to instructions
+    // TODO: implement helper method
     private void drawLine(Graphics g) {
-
+        double directionRadians = Math.toRadians(direction);
+        int xPixelNew = (int) Math.round(xPixel + Math.cos(directionRadians) * this.size);
+        int yPixelNew = (int) Math.round(yPixel + Math.sin(directionRadians) * this.size);
+        g.drawLine(xPixel, yPixel, xPixelNew, yPixelNew);
+        xPixel = xPixelNew;
+        yPixel = yPixelNew;
     }
 
-    // TODO: implement helper method according to instructions
+    // TODO: implement helper method
     private void rotate(int degrees) {
+        direction += degrees;
     }
 
-    // TODO: implement drawCurve according to instructions
+    // TODO: implement drawCurve
     private void drawCurve(Graphics g, int depth) {
-
+        if (depth == 0)
+            drawLine(g);
+        else {
+            drawCurve(g, depth - 1);
+            rotate(60);
+            drawCurve(g, depth - 1);
+            rotate(-120);
+            drawCurve(g, depth - 1);
+            rotate(60);
+            drawCurve(g, depth - 1);
+        }
     }
 
-    // TODO: implement drawSnowflake according to instructions
+    // TODO: implement drawSnowflake
     private void drawSnowflake(Graphics g) {
-
+        drawCurve(g, depth);
+        rotate(-120);
+        drawCurve(g, depth);
+        rotate(-120);
+        drawCurve(g, depth);
     }
 
     @Override
