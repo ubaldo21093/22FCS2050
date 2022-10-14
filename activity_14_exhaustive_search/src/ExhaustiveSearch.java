456c45e86c723c285f75663125fec05cdabf9db6
Thyago Mota <thyagomota@gmail.com>
2022-10-14 14:46:28 -0600

    updates

--- activity_14_exhaustive_search/src/ExhaustiveSearch.java
+++ activity_14_exhaustive_search/src/ExhaustiveSearch.java
@@ -1,5 +1,5 @@
 /*
- * CS 2050 - Computer Science II - Fall 2022
+ * CS 2050 - Computer Science II - Summer 2021
  * Instructor: Thyago Mota
  * Description: Activity 14 - Exhaustive Search
  */
@@ -12,20 +12,28 @@ public class ExhaustiveSearch {
     static void search(int n) {
 
         // TODO: create a stack of String objects
+        DynamicStack stack = new DynamicStack();
 
         // TODO: push all letters of the alphabet onto the stack
-
+        for (int i = ALPHABET.length() - 1; i >= 0; i--)
+            stack.push(ALPHABET.charAt(i) + "");
 
         // TODO: loop until the stack is empty
+        while (!stack.isEmpty()) {
 
             // TODO: pop a String from the stack
+            String s = stack.pop();
 
             // TODO: IF size of the String is “n”, print the String
-
+            if (s.length() == n)
+                System.out.println(s);
 
             // TODO: ELSE, push back all of the combinations of the String with each individual letters of the alphabet
-
-
+            else {
+                for (int i = ALPHABET.length() - 1; i >= 0; i--)
+                    stack.push(s + ALPHABET.charAt(i));
+            }
+        }
     }
 
     public static void main(String[] args) {
