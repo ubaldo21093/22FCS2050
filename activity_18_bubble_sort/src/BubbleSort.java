b21308887d5b88703c857e92c3ae80273c628509
Thyago Mota <thyagomota@gmail.com>
2022-10-25 15:41:36 -0600

    updates

---
+++ activity_18_bubble_sort/src/BubbleSort.java
@@ -0,0 +1,23 @@
+/*
+ * CS 2050 - Computer Science II - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 18 - Bubble Sort
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class BubbleSort {
+
+    // TODO: implement the bubble sort algorithm
+    public static void bubbleSort(int data[]) {
+
+    }
+
+    public static void main(String[] args) {
+        int data[] = {13, 12, 84, 79, 10, 77, 56, 1, 34, 27, 3};
+        System.out.println(Arrays.toString(data));
+        bubbleSort(data);
+        System.out.println(Arrays.toString(data));
+    }
+}
---
+++ activity_19_selection_sort/src/SelectionSort.java
@@ -0,0 +1,24 @@
+/*
+ * CS 2050 - Computer Science II - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 19 - Selection Sort
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SelectionSort {
+
+    // TODO: implement the selection sort algorithm
+    public static void selectionSort(int data[]) {
+
+    }
+
+    public static void main(String[] args) {
+
+        int data[] = {13, 12, 84, 79, 10, 77, 56, 1, 34, 27, 3};
+        System.out.println(Arrays.toString(data));
+        selectionSort(data);
+        System.out.println(Arrays.toString(data));
+    }
+}
