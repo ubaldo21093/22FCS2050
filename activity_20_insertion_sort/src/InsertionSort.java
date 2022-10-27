e4bde573158560e7ec1662aedfc9466341912927
Thyago Mota <thyagomota@gmail.com>
2022-10-27 17:26:35 -0600

    updates

---
+++ activity_20_insertion_sort/src/InsertionSort.java
@@ -0,0 +1,24 @@
+/*
+ * CS 2050 - Computer Science II - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 20 - Insertion Sort
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class InsertionSort {
+
+    // TODO: implement the insertion sort algorithm
+    public static void insertionSort(int data[]) {
+
+    }
+
+    public static void main(String[] args) {
+
+        int data[] = {13, 12, 84, 79, 10, 77, 56, 1, 34, 27, 3};
+        System.out.println(Arrays.toString(data));
+        insertionSort(data);
+        System.out.println(Arrays.toString(data));
+    }
+}
