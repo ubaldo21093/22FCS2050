522d7cc80786411cdc7746cc5ea9761a548884b6
Thyago Mota <thyagomota@gmail.com>
2022-11-01 10:55:06 -0600

    updates

---
+++ activity_21_merge_sort/src/MergeSort.java
@@ -0,0 +1,35 @@
+/*
+ * CS 2050 - Computer Science II - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 21 - Merge Sort
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class MergeSort {
+
+    // TODO: implement the merge portion of merge sort
+    public static void merge(int data[], int begin, int middle, int end) {
+        
+    }
+
+    // TODO: implement the merge sort algorithm
+    public static void mergeSort(int data[], int begin, int end) {
+        // base case
+        
+
+        // divide
+        
+
+        // conquer (merge)
+    }
+
+    public static void main(String[] args) {
+
+        int data[] = {13, 12, 84, 79, 10, 77, 56, 1, 34, 27, 3};
+        System.out.println(Arrays.toString(data));
+        mergeSort(data, 0, data.length - 1);
+        System.out.println(Arrays.toString(data));
+    }
+}
