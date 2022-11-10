b5508ca5f1aa9012f629aa5a31831ca19bea5e68
Thyago Mota <thyagomota@gmail.com>
2022-11-10 16:23:36 -0700

    updates

--- activity_22_bst/src/BST.java
+++ activity_22_bst/src/BST.java
@@ -43,16 +43,83 @@ public class BST<E extends Comparable<E>> {
     // TODO: override the toString method using a breadth first tree traversal using a queue of binary nodes
     @Override
     public String toString() {
-        return "";
+        //return postOrder(root);
+        String out = "";
+        Queue<BSTNode<E>> qu = new Queue<>();
+        if (root != null) {
+            qu.push(root);
+            while (!qu.isEmpty()) {
+                try {
+                    BSTNode<E> temp = qu.pop();
+                    out += temp + " ";
+                    if (temp.getLeft() != null)
+                        qu.push(temp.getLeft());
+                    if (temp.getRight() != null)
+                        qu.push(temp.getRight());
+                }
+                catch (Exception e) { }
+            } // end while
+        }
+        return out;
     }
 
     // TODO: implement searchRecursively
     private boolean searchRecursively(final BSTNode<E> current, final E value) {
-        return false;
+        if (value.compareTo(current.getValue()) == 0)
+            return true;
+        else if (value.compareTo(current.getValue()) < 0) {
+            if (current.getLeft() == null)
+                return false;
+            else
+                return searchRecursively(current.getLeft(), value);
+        } else {
+            if (current.getRight() == null)
+                return false;
+            else
+                return searchRecursively(current.getRight(), value);
+        }
     }
 
     // TODO: boolean search
     public boolean search(final E value) {
-        return false;
+        if (isEmpty())
+            return false;
+        else
+            return searchRecursively(root, value);
     }
+
+    public String inOrder(final BSTNode<E> current) {
+        if (current == null)
+            return "";
+        return inOrder(current.getLeft()) + " " + current + " " + inOrder(current.getRight());
+    }
+
+    public String preOrder(final BSTNode<E> current) {
+        if (current == null)
+            return "";
+        return current + " " + preOrder(current.getLeft()) + " " + preOrder(current.getRight());
+    }
+
+    public String postOrder(final BSTNode<E> current) {
+        if (current == null)
+            return "";
+        return postOrder(current.getLeft()) + " " + postOrder(current.getRight()) + " " + current;
+    }
+
+    private int numberChildren(final BSTNode<E> current) {
+        return 0;
+    }
+
+    private BSTNode<E> getLeftMost(final BSTNode<E> current) {
+        return null;
+    }
+
+    public BSTNode<E> removeRecursively(final BSTNode<E> current, final E value) {
+        return null;
+    }
+
+    public void remove(final E value) {
+
+    }
+
 }
