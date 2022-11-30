ea37c5b5dd13786b5b8f326ef54977ef6fe37d1b
Thyago Mota <thyagomota@gmail.com>
2022-11-29 17:01:26 -0700

    updates

--- activity_23_heap/src/Heap.java
+++ activity_23_heap/src/Heap.java
@@ -26,7 +26,12 @@ public class Heap<E extends Comparable<E>> {
     // TODO #1 implement the sift up method
     private void siftUp(int index) {
         int parent = parentOf(index);
-
+        if (data[index].compareTo(data[parent]) > 0) {
+            E temp = data[index];
+            data[index] = data[parent];
+            data[parent] = temp;
+            siftUp(parent);
+        }
     }
 
     // TODO #2: implement the sift down method
@@ -34,11 +39,35 @@ public class Heap<E extends Comparable<E>> {
         int left = 2 * index + 1;
         int right = left + 1;
         // case 1: has left and right
-
+        if (left < size && right < size) {
+            int greater = left;
+            if (data[greater].compareTo(data[right]) < 0)
+                greater = right;
+            if (data[index].compareTo(data[greater]) < 0) {
+                E temp = data[index];
+                data[index] = data[greater];
+                data[greater] = temp;
+                siftDown(greater);
+            }
+        }
         // case 2: only has left
-
+        else if (left < size) {
+            if (data[index].compareTo(data[left]) < 0) {
+                E temp = data[index];
+                data[index] = data[left];
+                data[left] = temp;
+                siftDown(left);
+            }
+        }
         // case 3: only has right
-
+        else if (right < size) {
+            if (data[index].compareTo(data[right]) < 0) {
+                E temp = data[index];
+                data[index] = data[right];
+                data[right] = temp;
+                siftDown(right);
+            }
+        }
         // case 4: no children -> do nothing!
     }
 
@@ -71,6 +100,22 @@ public class Heap<E extends Comparable<E>> {
 
     public static void main(String[] args) throws Exception {
         // TODO #3: instantiate a heap and add the following elements: 18, 13, 5, 10, 1, 2, 20; then display the heap using toString
+        Heap<Integer> heap = new Heap<>();
+        heap.add(18);
+        heap.add(13);
+        heap.add(5);
+        heap.add(10);
+        heap.add(1);
+        heap.add(2);
+        heap.add(20);
+        System.out.println(heap);
 
+        while (true) {
+            try {
+                System.out.println("[" + heap.remove() + "] " + heap);
+            } catch (Exception ex) {
+                break;
+            }
+        }
     }
 }
