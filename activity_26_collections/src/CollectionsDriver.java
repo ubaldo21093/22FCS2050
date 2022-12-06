fee5020dac41a3bc897f29d0a9e94c95329cdbbe
Thyago Mota <thyagomota@gmail.com>
2022-12-06 15:44:09 -0700

    updates

---
+++ activity_26_collections/src/CollectionsDriver.java
@@ -0,0 +1,48 @@
+/*
+ * CS2050 - Computer Science II - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 26 - CollectionsDriver
+ */
+
+import java.util.*;
+
+public class CollectionsDriver {
+
+    public static void main(String[] args) {
+
+        // TODO: create a HashMap with the following associations:
+        // 511:Joe, 923:Carla, 728:Paula, 298:Zion
+        // print the HashMap using its toString implementation
+        // note that the order or insertion (or of the keys) is not preserved
+
+
+        // TODO: now create a LinkedHashMap with the same associations
+        // note than now the order of insertion is preserved
+
+
+        // TODO: now create a TreeMap with the same associations
+        // note that the order of the keys is now preserved
+
+
+        // TODO: create a LinkedList with the following elements:
+        // Joe, Carla, Paula, Zion, Paula
+        // print the LinkedList using its toString implementation
+        // note that duplicates are allowed
+
+
+        // TODO: now create a HashSet with the same elements
+        // note that duplicates are not allowed anymore
+        // also note that the order or insertion (or of the keys) is not preserved
+
+
+        // TODO: now create a LinkedHashSet with the same elements
+        // note that duplicates are still not allowed
+        // also note that now the order of insertion is preserved
+
+
+        // TODO: finally create a TreeSet with the same elements
+        // note that duplicates are still not allowed
+        // also note that now the elements are sorted
+        
+    }
+}
